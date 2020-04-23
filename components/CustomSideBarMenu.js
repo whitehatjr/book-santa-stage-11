4966ceb229710693e1d9ba986a4a00222e45fed0
vishalgaddam873 <vishal18@navgurukul.org>
2020-04-23 22:30:00 +0530

    Image picker is done

--- components/CustomSideBarMenu.js
+++ components/CustomSideBarMenu.js
@@ -1,134 +1,165 @@
-import React, { Component} from 'react';
-import {StyleSheet, View, Text,TouchableOpacity, ImageBackground} from 'react-native';
-import { DrawerItems} from 'react-navigation-drawer'
-import {Avatar} from 'react-native-elements';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-import firebase from 'firebase';
-import db from '../config'
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  Platform,
+} from "react-native";
+import { DrawerItems } from "react-navigation-drawer";
+import { Avatar } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import firebase from "firebase";
+import db from "../config";
+import axios from "axios";
 
-export default class CustomSideBarMenu extends Component{
+export default class CustomSideBarMenu extends Component {
   state = {
-      userId : firebase.auth().currentUser.email,
-     image: null,
-     name: "",
-     docId : ""
-   };
+    userId: firebase.auth().currentUser.email,
+    image: "#",
+    name: "",
+    docId: "",
+  };
 
-   selectPicture = async () => {
-     const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
-       mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-     });
+  selectPicture = async () => {
+    const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-     if (!cancelled) this. updateProfilePicture(uri);
+    if (!cancelled) {
+      this.uploadImage(uri, this.state.userId);
+    }
+  };
 
-     // Points to the root reference
-     //var file = uri.createObjectURL()
-     const file = new Blob([JSON.stringify(uri, null, 2)], {type : 'application/json'});
-     console.log(file)
-   var storageRef = firebase.storage().ref('user_profiles/'+ file);
+  uploadImage = async (uri, imageName) => {
+    var response = await fetch(uri);
+    var blob = await response.blob();
 
-   storageRef.put(file)
-   }
+    var ref = firebase
+      .storage()
+      .ref()
+      .child("user_profiles/" + imageName);
 
-   // Create a root reference
+    return ref.put(blob).then((response) => {
+      this.fetchImage(imageName);
+    });
+  };
 
+  fetchImage = (imageName) => {
+    var storageRef = firebase
+      .storage()
+      .ref()
+      .child("user_profiles/" + imageName);
 
+    // Get the download URL
+    storageRef
+      .getDownloadURL()
+      .then((url) => {
+        this.setState({ image: url });
+      })
+      .catch((error) => {
+        this.setState({ image: "#" });
+      });
+  };
 
+  getUserProfile() {
+    db.collection("users")
+      .where("email_id", "==", this.state.userId)
+      .onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.setState({
+            name: doc.data().first_name + " " + doc.data().last_name,
+            docId: doc.id,
+            image: doc.data().image,
+          });
+        });
+      });
+  }
 
+  componentDidMount() {
+    this.fetchImage(this.state.userId);
+    this.getUserProfile();
+  }
 
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flex: 0.5,
 
-   updateProfilePicture=(uri)=>{
-     db.collection('users').doc(this.state.docId).update({
-       image : uri
-     })
-   }
+            alignItems: "center",
+            backgroundColor: "orange",
+          }}
+        >
+          <Avatar
+            rounded
+            source={{
+              uri: this.state.image,
+            }}
+            size="medium"
+            onPress={() => this.selectPicture()}
+            containerStyle={styles.imageContainer}
+            showEditButton
+          />
 
-   getUserProfile(){
-     db.collection('users')
-     .where('email_id','==',this.state.userId)
-     .onSnapshot(querySnapshot => {
-       querySnapshot.forEach(doc => {
-         this.setState({
-           name : doc.data().first_name + " " + doc.data().last_name,
-           docId : doc.id,
-           image : doc.data().image
-         })
-       })
-     })
-   }
-
-
-componentDidMount(){
-  this.getUserProfile()
-
-}
-
-  render(){
-    return(
-      <View style={{flex:1}}>
-        <View style={{flex:0.5,borderColor:'red',borderWidth:2,alignItems:'center',backgroundColor:'orange'}}>
-
-            <Avatar
-                rounded
-                icon={{name: 'user', type: 'font-awesome'}}
-                source={{
-                  uri:
-                    this.state.image
-                  }}
-                size="medium"
-
-                 overlayContainerStyle={{backgroundColor: 'white'}}
-                 onPress={() => this.selectPicture()}
-                 activeOpacity={0.7}
-                 containerStyle={{flex:0.75,width:'40%',height:'20%', marginLeft: 20, marginTop: 30,borderRadius:40}}
-              />
-
-
-            <Text style = {{fontWeight:'100',fontSize:20,paddingTop:10,}}> {this.state.name}</Text>
-
+          <Text style={{ fontWeight: "100", fontSize: 20, paddingTop: 10 }}>
+            {this.state.name}
+          </Text>
         </View>
 
         <View style={styles.drawerItemsContainer}>
-          <DrawerItems {...this.props}/>
+          <DrawerItems {...this.props} />
         </View>
         <View style={styles.logOutContainer}>
-          <TouchableOpacity style={styles.logOutButton}
-          onPress = {() => {
-              this.props.navigation.navigate('WelcomeScreen')
-              firebase.auth().signOut()
-          }}>
+          <TouchableOpacity
+            style={styles.logOutButton}
+            onPress={() => {
+              this.props.navigation.navigate("WelcomeScreen");
+              firebase.auth().signOut();
+            }}
+          >
             <Text>Log Out</Text>
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 }
 
 var styles = StyleSheet.create({
-  container : {
-    flex:1
+  container: {
+    flex: 1,
   },
-  drawerItemsContainer:{
-    flex:0.8
+  drawerItemsContainer: {
+    flex: 0.8,
   },
-  logOutContainer : {
-    flex:0.2,
-    justifyContent:'flex-end',
-    paddingBottom:30
+  logOutContainer: {
+    flex: 0.2,
+    justifyContent: "flex-end",
+    paddingBottom: 30,
   },
-  logOutButton : {
-    height:30,
-    width:'100%',
-    justifyContent:'center',
-    padding:10
+  logOutButton: {
+    height: 30,
+    width: "100%",
+    justifyContent: "center",
+    padding: 10,
   },
-  logOutText:{
+  imageContainer: {
+    flex: 0.75,
+    width: "40%",
+    height: "20%",
+    marginLeft: 20,
+    marginTop: 30,
+    borderRadius: 40,
+  },
+  logOutText: {
     fontSize: 30,
-    fontWeight:'bold'
-  }
-})
+    fontWeight: "bold",
+  },
+});
